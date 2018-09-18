e8ca34f44879b0fe12580ca71f6284c96f2dd1d7
Alberto Bezerra <albertofbezerra@gmail.com>
2018-09-18 01:07:54 -0300

    Alberto Bezerra
    Primeira atualizacao

---
+++ footer.php
@@ -0,0 +1,56 @@
+<?php wp_footer(); ?>
+
+<div class="footer">
+
+	<div class="container">
+		
+		<div class="row">
+
+			<div class="col-sm">
+				<div class="titulo"><b>SOBRE O CEM</b></div>
+					<div class="descricao">
+					Curabitur turpis. Vestibulum fringilla pede sit amet augue. In auctor lobortis lacus. Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus. Curabitur blandit mollis lacus.</div>
+				
+			</div>
+
+			<div class="col-sm">
+				<div class="titulo"><b>FALE COM O CEM</b></div>
+				<div class="descricao">Rua Mércia Maria, 89 - Bairro Universitário - Caruaru - PE<br>
+				Fone: (81) 3722-6727 / 98880-6727 / Whatsapp 99655-1102<br> 
+				Email: centroeducacionalmarluci@hotmail.com
+				</div>
+			</div>
+
+			<div class="col-sm">
+				<div class="fb-page" data-href="https://www.facebook.com/centroeducacionalmarluci/" data-tabs="timeline" data-height="120" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/centroeducacionalmarluci/" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/centroeducacionalmarluci/">Centro Educacional Marlucí</a></blockquote></div>
+			</div>
+
+		</div>
+
+	</div>
+
+
+</div>
+
+<div class="copyright">
+
+	<div class="container text-center">
+
+		<img  src="<?php bloginfo('template_directory'); ?>/assets/images/logo2.png" class="logo-footer">
+		<br><br><h6>© COPYRIGHT 2018</h6>
+
+	</div>
+
+</div>
+
+<div class="desenvolvimento">
+
+	<div class="container text-center">
+		<a href="www.equipebezerradasilva.com.br/alberto" class="link">Desenvolvido por Alberto da Equipe Bezerra da Silva</a>
+	</div>
+	
+</div>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,228 @@
+<?php get_header(); ?>
+
+<div class="slider">
+	
+			<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+				  <ol class="carousel-indicators">
+				    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+				  </ol>
+				  	 <div class="carousel-inner ">
+				    <div class="carousel-item active">
+				      <img class="d-block w-100" src="<?php bloginfo( 'template_directory' ); ?>/assets/images/01.jpg" alt="First slide">
+				    </div>
+				    <div class="carousel-item">
+				      <img class="d-block w-100" src="<?php bloginfo( 'template_directory' ); ?>/assets/images/02.jpg" alt="Second slide">
+				    </div>
+				    <div class="carousel-item">
+				      <img class="d-block w-100" src="<?php bloginfo( 'template_directory' ); ?>/assets/images/03.jpg" alt="Third slide">
+				    </div>
+				  </div>
+				  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+				    <span class="sr-only">Next</span>
+				  </a>
+		
+			</div>
+	
+</div>
+
+<div class="banner">
+
+	<div class="container">
+	
+	<img  src="<?php bloginfo('template_directory'); ?>/assets/images/banner.jpg" class="tamanho-banner rounded">
+
+	</div>
+
+</div>
+
+<div class="titulo-dicas">
+	<div class="container">
+
+		<ul class="nav">
+		  <li class="nav-item nav-item-dicas">
+		    Dicas do CEM
+		  </li>
+		</ul>
+	</div>
+
+</div>
+
+<div class="cards">
+	
+	<div class="container">
+		<div class="card-deck">
+		
+	   		<div class="card">
+				  <img class="card-img-top" src="<?php bloginfo( 'template_directory' ); ?>/assets/images/01.jpg" alt="First slide">
+				  <div class="card-body">
+				    <h5 class="card-title">Card title</h5>
+				    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				  </div>
+	   		</div>
+
+	   		<div class="card">
+				  <img class="card-img-top" src="<?php bloginfo( 'template_directory' ); ?>/assets/images/02.jpg" alt="First slide">
+				  <div class="card-body">
+				    <h5 class="card-title">Card title</h5>
+				    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				  </div>
+	   		</div>
+
+
+			<div class="card">
+				  <img class="card-img-top" src="<?php bloginfo( 'template_directory' ); ?>/assets/images/03.jpg" alt="First slide">
+				  <div class="card-body">
+				    <h5 class="card-title">Card title</h5>
+				    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				  </div>
+	   		</div>
+
+
+	  	</div>
+
+	  	<div class=" espacobtn">
+
+	  	<button type="button" class="btn-primary-custom">+ dicas</button>
+
+	  	</div>
+
+	</div>
+
+
+</div>
+
+
+<div class="agendaeaulasextra">
+	
+
+	<div class="container">
+  		<div class="row">
+
+
+
+		<div class="col-sm">
+
+				<ul class="nav">
+				  <li class="nav-item nav-item-dicas">
+				    Agenda do CEM
+				  </li>
+				</ul>
+
+				<div class="aligndobtn">
+
+				<button type="button" class="btn btn-primary btn-primary-custom">Primary</button>
+
+				</div>
+
+
+
+		</div>
+
+
+
+  		<div class="col-sm">
+
+
+				<ul class="nav">
+				  <li class="nav-item nav-item-dicas">
+				    Aulas Extracurrículares
+				  </li>
+				</ul>
+
+				<div class="caroseldasaulas">
+						<div id="caroseel01" class="carousel slide" data-ride="carousel">
+						
+						  <ol class="carousel-indicators">
+						    <li data-target="#caroseel01" data-slide-to="0" class="active"></li>
+						    <li data-target="#caroseel01" data-slide-to="1"></li>
+						    <li data-target="#caroseel01" data-slide-to="2"></li>
+						  </ol>
+						  	 <div class="carousel-inner ">
+						    <div class="carousel-item active">
+						      <img class="d-block w-100 rounded" src="<?php bloginfo( 'template_directory' ); ?>/assets/images/01.jpg" alt="First slide">
+						    </div>
+						    <div class="carousel-item">
+						      <img class="d-block w-100 rounded" src="<?php bloginfo( 'template_directory' ); ?>/assets/images/02.jpg" alt="Second slide">
+						    </div>
+						    <div class="carousel-item">
+						      <img class="d-block w-100 rounded" src="<?php bloginfo( 'template_directory' ); ?>/assets/images/03.jpg" alt="Third slide">
+						    </div>
+						  </div>
+						  <a class="carousel-control-prev" href="#caroseel01" role="button" data-slide="prev">
+						    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						    <span class="sr-only">Previous</span>
+						  </a>
+						  <a class="carousel-control-next" href="#caroseel01" role="button" data-slide="next">
+						    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+						    <span class="sr-only">Next</span>
+						  </a>
+		
+					</div>
+				</div>
+
+  			
+  		</div>
+
+
+  </div>
+
+</div>
+
+<div class="galeriadeimagens">
+	
+	<div class="container">
+
+
+		<ul class="nav">
+		  <li class="nav-item nav-item-dicas">
+		    Galerias do CEM
+		  </li>
+		</ul>
+
+	</div>
+
+
+	<div class="container">
+		<div class="card-deck">
+		
+	   		<div class="card">
+				  <img class="card-img-top" src="<?php bloginfo( 'template_directory' ); ?>/assets/images/01.jpg" alt="First slide">
+				  <div class="card-body">
+				    <h5 class="card-title">Card title</h5>
+				    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				  </div>
+	   		</div>
+
+	   		<div class="card">
+				  <img class="card-img-top" src="<?php bloginfo( 'template_directory' ); ?>/assets/images/02.jpg" alt="First slide">
+				  <div class="card-body">
+				    <h5 class="card-title">Card title</h5>
+				    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				  </div>
+	   		</div>
+
+
+			<div class="card">
+				  <img class="card-img-top" src="<?php bloginfo( 'template_directory' ); ?>/assets/images/03.jpg" alt="First slide">
+				  <div class="card-body">
+				    <h5 class="card-title">Card title</h5>
+				    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+				  </div>
+	   		</div>
+
+
+	  	</div>
+	  	<button type="button" class="btn btn-primary btn-primary-custom">Primary</button>
+	</div>
+
+
+</div>
+
+<?php get_footer(); ?>
