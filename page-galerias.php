18220d07fe2c0942d11063bb62be6f02fa2396dc
Alberto Bezerra <albertofbezerra@gmail.com>
2018-10-05 13:24:54 -0300

    Alberto Bezerra
    Página: Galeria de imagens dinâmicas

--- page-galerias.php
+++ page-galerias.php
@@ -2,31 +2,47 @@
 
 <div class="galerias">
 
-			<div class="container">
+	<div class="texto-titulo-da-galeria"><center><h3>Galerias do CEM</h3></center></div>
 
-				<div class="card-deck">
+
+	<div class="container">
+		<div class="row">
+
+
 
 					<?php 
-						$args = array('post_type'=>'post', 'category_name'=>'galeria', 'showposts'=> '4');
+						$args = array('post_type'=>'post', 'category_name'=>'galeria', 'showposts'=> '10');
 						$my_posts = get_posts( $args );
 						if($my_posts) : foreach($my_posts as $post) : setup_postdata( $post );
 					 ?>
-			
+
+				<div class="col-md-4 col-lg-4 espacodagaleria">
 				
-			   		<div class="card">
-						  <?php the_post_thumbnail(false, array('class'=>'card-img-top')); ?>
-						  <div class="card-body">
+					<div class="card">
+						  <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(false, array('class'=>'card-img-top')); ?></a>
+
+						  <a class="linkdagaleria" href="<?php the_permalink(); ?>"><div class="card-body">
 						    <h5 class="card-title"><?php the_title(); ?></h5>
 						    <p class="card-text"><?php the_excerpt(); ?></p>
-						  </div>
+						  </div></a>
 			   		</div>
+				
+				
+				</div>
+			
+			 <?php
+		    	endforeach;
+		    	endif;
+	     	?>
+		  
+		  </div>
 
-			   		<?php
-				    	endforeach;
-				    	endif;
-			     	?>
-			</div>
-		</div>
+
+
+
+
+	</div>
+</div>
 
 
 <?php get_footer(); ?>
