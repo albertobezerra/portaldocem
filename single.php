06f618d5368206011072d15d670fd70027a20641
Alberto Bezerra <albertofbezerra@gmail.com>
2018-10-06 16:53:12 -0300

    Alberto Bezerra
    Finalização da paginha sigle.php

--- single.php
+++ single.php
@@ -1,4 +1,14 @@
 <?php get_header(); ?>
+<div class="post">
+	<div class="container">
+
+			<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+				<center><h3><?php the_title(); ?></h3></center></br>
+				<?php the_content(); ?>
+			<?php endwhile; endif; ?>
+
+	</div>
+</div>
 
 
 <?php get_footer(); ?>
