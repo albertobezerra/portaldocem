4084d90c595d6268f88b71407ad6dfc434972f10
Alberto Bezerra <albertofbezerra@gmail.com>
2018-10-06 00:42:16 -0300

    Alberto Bezerra
    Galeria do mes com imagem do post da categoria > galeriadomes

--- index.php
+++ index.php
@@ -133,7 +133,19 @@
 				  </li>
 				</ul>
 
-				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
+				<?php 
+				$args = array('post_type'=>'post', 'category_name'=>'agendadomes', 'showposts'=> '1');
+				$my_posts = get_posts( $args );
+				if($my_posts) : foreach($my_posts as $post) : setup_postdata( $post );
+			 	?>
+
+				<?php the_post_thumbnail('resize')?>
+
+
+			 	<?php
+		    	endforeach;
+		    	endif;
+	     		?>
 
 				<div class="espacobtn">
 
