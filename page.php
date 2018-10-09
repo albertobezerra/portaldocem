cbac5a90661e4e78faabbd69f1913666f7532a01
Alberto Bezerra <albertofbezerra@gmail.com>
2018-10-08 23:39:58 -0300

    Alberto Bezerra
    Finalização da págona page.php

--- page.php
+++ page.php
@@ -1,15 +1,15 @@
 <?php get_header(); ?>
 
-<div class="container">
-	<div class="page">
-		
-		<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-			<?php the_content(); ?>
-		<?php endwhile; endif; ?>
+<div class="pagina">
+	<div class="container">
+
+			<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+				<center><h3><?php the_title(); ?></h3></center></br>
+				<?php the_content(); ?>
+			<?php endwhile; endif; ?>
 
 	</div>
 </div>
-
 
 
 <?php get_footer(); ?>
